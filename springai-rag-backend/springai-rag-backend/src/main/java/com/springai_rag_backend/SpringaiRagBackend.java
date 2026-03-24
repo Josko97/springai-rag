d05e5f8c00f6b2c8e515eeb74bd4e2b4b5849772
Josko97 <joskobarbir1988@gmail.com>
2026-03-24 21:52:25 +0100

    Initial monorepo structure: backend and frontend

---
+++ springai-rag-backend/springai-rag-backend/src/main/java/com/springai_rag_backend/SpringaiRagBackend.java
@@ -0,0 +1,13 @@
+package com.springai_rag_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiRagBackend {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiRagBackend.class, args);
+    }
+
+}
---
+++ springai-rag-backend/springai-rag-backend/src/test/java/com/springai_rag_backend/SpringaiRagBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai_rag_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiRagBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
